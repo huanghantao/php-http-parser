9fab39d13e76aa83d1fb77a78ab835072124eb8d
codinghuang <2812240764@qq.com>
2020-08-20 22:51:49 +0800

    feat: register Http\Parser class

--- php_http_parser.cc
+++ php_http_parser.cc
@@ -22,6 +22,7 @@
 #include "php/main/php.h"
 #include "php/ext/standard/info.h"
 #include "./php_http_parser.h"
+#include "thirdparty/http/http_parser.h"
 
 /* For compatibility with older PHP versions */
 #ifndef ZEND_PARSE_PARAMETERS_NONE
@@ -29,6 +30,31 @@
     ZEND_PARSE_PARAMETERS_START(0, 0)                                                                                  \
     ZEND_PARSE_PARAMETERS_END()
 #endif
+
+typedef struct {
+    http_parser *parser;
+    zend_object std;
+} http_parser_t;
+
+zend_class_entry *http_parser_ce;
+static zend_object_handlers http_parser_handlers;
+
+static inline http_parser_t *php_http_parser_fetch_object(zend_object *obj) {
+    return (http_parser_t *) ((char *) obj - http_parser_handlers.offset);
+}
+
+static zend_object *php_http_parser_create_object(zend_class_entry *ce) {
+    http_parser_t *http_parser = reinterpret_cast<http_parser_t *>(zend_object_alloc(sizeof(http_parser_t), ce));
+    zend_object_std_init(&http_parser->std, ce);
+    object_properties_init(&http_parser->std, ce);
+    http_parser->std.handlers = &http_parser_handlers;
+    return &http_parser->std;
+}
+
+static void php_http_parser_free_object(zend_object *object) {
+    http_parser_t *http_parser = reinterpret_cast<http_parser_t *>(php_http_parser_fetch_object(object));
+    zend_object_std_dtor(&http_parser->std);
+}
 
 /* {{{ void php_http_parser_test1()
  */
@@ -57,9 +83,19 @@ PHP_FUNCTION(php_http_parser_test2) {
 }
 /* }}}*/
 
+static const zend_function_entry http_parser_methods[] = {};
+
+PHP_MINIT_FUNCTION(http_parser) {
+    HTTP_PARSER_INIT_CLASS_ENTRY(http_parser, "Http\\Parser", NULL, NULL, http_parser_methods);
+    HTTP_PARSER_SET_CLASS_CUSTOM_OBJECT(
+        http_parser, php_http_parser_create_object, php_http_parser_free_object, http_parser_t, std);
+
+    return SUCCESS;
+}
+
 /* {{{ PHP_RINIT_FUNCTION
  */
-PHP_RINIT_FUNCTION(php_http_parser) {
+PHP_RINIT_FUNCTION(http_parser) {
 #if defined(ZTS) && defined(COMPILE_DL_php_http_parser)
     ZEND_TSRMLS_CACHE_UPDATE();
 #endif
@@ -70,7 +106,7 @@ PHP_RINIT_FUNCTION(php_http_parser) {
 
 /* {{{ PHP_MINFO_FUNCTION
  */
-PHP_MINFO_FUNCTION(php_http_parser) {
+PHP_MINFO_FUNCTION(http_parser) {
     char buf[64];
 
     php_info_print_table_start();
@@ -96,7 +132,7 @@ ZEND_END_ARG_INFO()
 
 /* {{{ php_http_parser_functions[]
  */
-static const zend_function_entry php_http_parser_functions[] = {
+static const zend_function_entry http_parser_functions[] = {
     PHP_FE(php_http_parser_test1, arginfo_php_http_parser_test1)
         PHP_FE(php_http_parser_test2, arginfo_php_http_parser_test2) PHP_FE_END};
 /* }}} */
@@ -104,14 +140,14 @@ static const zend_function_entry php_http_parser_functions[] = {
 /* {{{ http_parser_module_entry
  */
 zend_module_entry http_parser_module_entry = {STANDARD_MODULE_HEADER,
-                                              "http_parser",              /* Extension name */
-                                              php_http_parser_functions,  /* zend_function_entry */
-                                              NULL,                       /* PHP_MINIT - Module initialization */
-                                              NULL,                       /* PHP_MSHUTDOWN - Module shutdown */
-                                              PHP_RINIT(php_http_parser), /* PHP_RINIT - Request initialization */
-                                              NULL,                       /* PHP_RSHUTDOWN - Request shutdown */
-                                              PHP_MINFO(php_http_parser), /* PHP_MINFO - Module info */
-                                              PHP_HTTP_PARSER_VERSION,    /* Version */
+                                              "http_parser",           /* Extension name */
+                                              http_parser_functions,   /* zend_function_entry */
+                                              PHP_MINIT(http_parser),  /* PHP_MINIT - Module initialization */
+                                              NULL,                    /* PHP_MSHUTDOWN - Module shutdown */
+                                              PHP_RINIT(http_parser),  /* PHP_RINIT - Request initialization */
+                                              NULL,                    /* PHP_RSHUTDOWN - Request shutdown */
+                                              PHP_MINFO(http_parser),  /* PHP_MINFO - Module info */
+                                              PHP_HTTP_PARSER_VERSION, /* Version */
                                               STANDARD_MODULE_PROPERTIES};
 /* }}} */
 
--- php_http_parser.h
+++ php_http_parser.h
@@ -23,6 +23,135 @@ extern zend_module_entry http_parser_module_entry;
 
 #define PHP_HTTP_PARSER_VERSION "0.1.0"
 
+#define php_http_parser_array_get_value(ht, str, v) ((v = zend_hash_str_find(ht, str, sizeof(str)-1)) && !ZVAL_IS_NULL(v))
+
+#define HTTP_PARSER_SET_CLASS_CREATE(module, _create_object) \
+    module##_ce->create_object = _create_object
+
+#define HTTP_PARSER_SET_CLASS_FREE(module, _free_obj) \
+    module##_handlers.free_obj = _free_obj
+
+#define HTTP_PARSER_SET_CLASS_CREATE_AND_FREE(module, _create_object, _free_obj) \
+    HTTP_PARSER_SET_CLASS_CREATE(module, _create_object); \
+    HTTP_PARSER_SET_CLASS_FREE(module, _free_obj)
+
+#define HTTP_PARSER_SET_CLASS_CUSTOM_OBJECT(module, _create_object, _free_obj, _struct, _std) \
+    HTTP_PARSER_SET_CLASS_CREATE_AND_FREE(module, _create_object, _free_obj); \
+    module##_handlers.offset = XtOffsetOf(_struct, _std)
+
+#define HTTP_PARSER_Z_OBJCE_NAME_VAL_P(zobject) ZSTR_VAL(Z_OBJCE_P(zobject)->name)
+
+#if PHP_VERSION_ID < 70300
+/* Allocates object type and zeros it, but not the properties.
+* Properties MUST be initialized using object_properties_init(). */
+static inline void *zend_object_alloc(size_t obj_size, zend_class_entry *ce)
+{
+    void *obj = emalloc(obj_size + zend_object_properties_size(ce));
+    /* Subtraction of sizeof(zval) is necessary, because zend_object_properties_size() may be
+    * -sizeof(zval), if the object has no properties. */
+    memset(obj, 0, obj_size - sizeof(zval));
+    return obj;
+}
+#endif
+
+/* PHP 7 class declaration macros */
+
+static inline int http_parser_zend_register_class_alias(const char *name, size_t name_len, zend_class_entry *ce)
+{
+    zend_string *_name;
+    if (name[0] == '\\')
+    {
+        _name = zend_string_init(name, name_len, 1);
+        zend_str_tolower_copy(ZSTR_VAL(_name), name + 1, name_len - 1);
+    }
+    else
+    {
+        _name = zend_string_init(name, name_len, 1);
+        zend_str_tolower_copy(ZSTR_VAL(_name), name, name_len);
+    }
+
+    zend_string *_interned_name = zend_new_interned_string(_name);
+
+#if PHP_VERSION_ID >= 70300
+    return zend_register_class_alias_ex(ZSTR_VAL(_interned_name), ZSTR_LEN(_interned_name), ce, 1);
+#else
+    return zend_register_class_alias_ex(ZSTR_VAL(_interned_name), ZSTR_LEN(_interned_name), ce);
+#endif
+}
+
+#define HTTP_PARSER_CLASS_ALIAS(name, module) do { \
+    if (name) { \
+        http_parser_zend_register_class_alias(ZEND_STRL(name), module##_ce); \
+    } \
+} while (0)
+
+#define HTTP_PARSER_CLASS_ALIAS_SHORT_NAME(shortName, module) do { \
+        HTTP_PARSER_CLASS_ALIAS(shortName, module); \
+} while (0)
+
+#define HTTP_PARSER_SET_CLASS_SERIALIZABLE(module, _serialize, _unserialize) \
+    module##_ce->serialize = _serialize; \
+    module##_ce->unserialize = _unserialize
+
+#define http_parser_zend_class_clone_deny NULL
+#define HTTP_PARSER_SET_CLASS_CLONEABLE(module, _clone_obj) \
+    module##_handlers.clone_obj = _clone_obj
+
+#define HTTP_PARSER_SET_CLASS_UNSET_PROPERTY_HANDLER(module, _unset_property) \
+    module##_handlers.unset_property = _unset_property
+
+#define HTTP_PARSER_INIT_CLASS_ENTRY_BASE(module, namespaceName, snake_name, shortName, methods, parent_ce) do { \
+    zend_class_entry _##module##_ce; \
+    INIT_CLASS_ENTRY(_##module##_ce, namespaceName, methods); \
+    module##_ce = zend_register_internal_class_ex(&_##module##_ce, parent_ce); \
+    HTTP_PARSER_CLASS_ALIAS(snake_name, module); \
+    HTTP_PARSER_CLASS_ALIAS_SHORT_NAME(shortName, module); \
+} while (0)
+
+#define HTTP_PARSER_INIT_CLASS_ENTRY(module, namespaceName, snake_name, shortName, methods) \
+    HTTP_PARSER_INIT_CLASS_ENTRY_BASE(module, namespaceName, snake_name, shortName, methods, NULL); \
+    memcpy(&module##_handlers, zend_get_std_object_handlers(), sizeof(zend_object_handlers))
+
+#define HTTP_PARSER_INIT_CLASS_ENTRY_EX(module, namespaceName, snake_name, shortName, methods, parent_module) \
+    HTTP_PARSER_INIT_CLASS_ENTRY_BASE(module, namespaceName, snake_name, shortName, methods, parent_module##_ce); \
+    memcpy(&module##_handlers, &parent_module##_handlers, sizeof(zend_object_handlers))
+
+#define HTTP_PARSER_INIT_CLASS_ENTRY_EX2(module, namespaceName, snake_name, shortName, methods, parent_module_ce, parent_module_handlers) \
+    HTTP_PARSER_INIT_CLASS_ENTRY_BASE(module, namespaceName, snake_name, shortName, methods, parent_module_ce); \
+    memcpy(&module##_handlers, parent_module_handlers, sizeof(zend_object_handlers))
+
+#if PHP_VERSION_ID < 80000
+static inline void http_parser_zend_class_unset_property_deny(zval *zobject, zval *zmember, void **cache_slot)
+{
+    zend_class_entry *ce = Z_OBJCE_P(zobject);
+    while (ce->parent)
+    {
+        ce = ce->parent;
+    }
+    if (EXPECTED(zend_hash_find(&ce->properties_info, Z_STR_P(zmember))))
+    {
+        zend_throw_error(NULL, "Property %s of class %s cannot be unset", Z_STRVAL_P(zmember), HTTP_PARSER_Z_OBJCE_NAME_VAL_P(zobject));
+        return;
+    }
+    std_object_handlers.unset_property(zobject, zmember, cache_slot);
+}
+#else
+static inline void http_parser_zend_class_unset_property_deny(zend_object *object, zend_string *member, void **cache_slot)
+{
+    zend_class_entry *ce = object->ce;
+    while (ce->parent)
+    {
+        ce = ce->parent;
+    }
+    if (EXPECTED(zend_hash_find(&ce->properties_info, member)))
+    {
+        zend_throw_error(NULL, "Property %s of class %s cannot be unset", ZSTR_VAL(member), ZSTR_VAL(object->ce->name));
+        return;
+    }
+    std_object_handlers.unset_property(object, member, cache_slot);
+}
+#endif
+
 #if defined(ZTS) && defined(COMPILE_DL_PHP_HTTP_PARSER)
 ZEND_TSRMLS_CACHE_EXTERN()
 #endif
