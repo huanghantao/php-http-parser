a76fb0e4be65cfad1fdfb58c2885ac3019d2530c
codinghuang <2812240764@qq.com>
2020-08-21 01:30:05 +0800

    feat: add PHP_METHOD(http_request_parser, getHeaders)

--- php_http_request_parser.cc
+++ php_http_request_parser.cc
@@ -33,7 +33,7 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_http_request_parser_void, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_http_request_parser_parse, 0, 0, 1)
-    ZEND_ARG_INFO(0, data)
+ZEND_ARG_INFO(0, data)
 ZEND_END_ARG_INFO()
 
 static inline http_request_parser_t *php_http_request_parser_fetch_object(zend_object *obj) {
@@ -56,8 +56,7 @@ static void php_http_request_parser_free_object(zend_object *object) {
     zend_object_std_dtor(&http_request_parser->std);
 }
 
-static Ctx *php_http_request_parser_get_ptr(zval *zobject)
-{
+static Ctx *php_http_request_parser_get_ptr(zval *zobject) {
     return php_http_request_parser_fetch_object(Z_OBJ_P(zobject))->ctx;
 }
 
@@ -71,23 +70,41 @@ static PHP_METHOD(http_request_parser, __construct) {
 static PHP_METHOD(http_request_parser, parse) {
     char *data;
     size_t data_len;
+
     ZEND_PARSE_PARAMETERS_START(1, 1)
-        Z_PARAM_STRING(data, data_len)
+    Z_PARAM_STRING(data, data_len)
     ZEND_PARSE_PARAMETERS_END_EX(RETURN_FALSE);
 
     Ctx *ctx = php_http_request_parser_get_ptr(ZEND_THIS);
     /* Start up / continue the parser.
-    * Note we pass recved==0 to signal that EOF has been received.
-    */
+     * Note we pass recved==0 to signal that EOF has been received.
+     */
     size_t nparsed = ctx->parse(data, data_len);
     RETURN_LONG(nparsed);
 }
 
+static PHP_METHOD(http_request_parser, getHeaders) {
+    Ctx *ctx = php_http_request_parser_get_ptr(ZEND_THIS);
+
+    array_init(return_value);
+
+    for (auto iter = ctx->request->header.begin(); iter != ctx->request->header.end(); iter++) {
+        add_assoc_stringl_ex(
+            return_value, iter->first.c_str(), iter->first.length(), (char *) iter->second.c_str(), iter->second.length());
+    }
+    RETURN_ARR(Z_ARRVAL_P(return_value));
+}
+
+static PHP_METHOD(http_request_parser, getBody) {
+    Ctx *ctx = php_http_request_parser_get_ptr(ZEND_THIS);
+    RETURN_STRINGL(ctx->request->body, ctx->request->body_length);
+}
+
 static const zend_function_entry http_request_parser_methods[] = {
     PHP_ME(http_request_parser, __construct, arginfo_http_request_parser_void, ZEND_ACC_PUBLIC)
-    PHP_ME(http_request_parser, parse, arginfo_http_request_parser_parse, ZEND_ACC_PUBLIC)
-    PHP_FE_END
-};
+        PHP_ME(http_request_parser, parse, arginfo_http_request_parser_parse, ZEND_ACC_PUBLIC)
+            PHP_ME(http_request_parser, getHeaders, arginfo_http_request_parser_void, ZEND_ACC_PUBLIC)
+                PHP_ME(http_request_parser, getBody, arginfo_http_request_parser_void, ZEND_ACC_PUBLIC) PHP_FE_END};
 
 void php_http_request_parser_minit(int module_number) {
     HTTP_PARSER_INIT_CLASS_ENTRY(http_request_parser, "Http\\Request\\Parser", NULL, NULL, http_request_parser_methods);
